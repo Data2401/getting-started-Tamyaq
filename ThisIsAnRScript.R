0815d6dfd273f460edd932989b8bc49bca19cc6b
Tamana Yaqoubi <TamYaqCol@gmail.com>
2025-08-20 11:59:28 -0500

    changed 18 to 20 for x changed 4 to 2

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,8 +1,8 @@
 ## This is a R script 
 ## Run this code to make a plot of x and x^2 for the integers from 1 to 10
 
-x <- 1:18
-y <- x^4
+x <- 1:20
+y <- x^2
 
 
 plot(x,y, type = "b", main = "Cubed integers")
